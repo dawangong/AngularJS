ef3b944cd6c7a9f5fb2892f33e2ea85128e36f1e
dawangong <dawangong@126.com>
2018-02-13 09:21:08 +0800

    update

--- test.js
+++ test.js
@@ -6,18 +6,18 @@ var mainApp = angular.module("mainApp", []);
 mainApp.config(function($provide) {
 
 
-   $provide.provider('MathService', function() {
-   	
-      this.$get = function() {
-         var factory = {};  
-         
-         factory.multiply = function(a, b) {
-            return a * b; 
-         };
-         return factory;
-      };
+    $provide.provider('MathService', function() {
 
-   });
+        this.$get = function() {
+            var factory = {};
+
+            factory.multiply = function(a, b) {
+                return a * b;
+            };
+            return factory;
+        };
+
+    });
 
 
 
