16d4dbe2cf9057744ec4d7ba6224902ba2667cac
dawangong <dawangong@126.com>
2018-03-26 15:46:09 +0800

    update

--- component/test2/index.js
+++ component/test2/index.js
@@ -1,9 +1,16 @@
 import angular from '../../js/angular';
 import controller from './tabCtrl';
+import template from './tab.tpl.html';
 
-const ddo = {
+const tabDDO = {
 	restrict: 'E',
-	controller: controller
+	controller: controller,
+	controllerAs: '$ctrl',
+	transclude: true,
+	bindings:{
+		attr: '<set'
+	},
+	template: template
 };
 
-export default angular.module('component', []).directive('tab', () => ddo).name;
+export default angular.module('component', []).component('tab', tabDDO).name;
