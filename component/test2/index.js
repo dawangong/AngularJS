31e74d4e0515caa1886c3aa2142d62fc6b9cfc0b
dawangong <dawangong@126.com>
2018-03-26 14:04:01 +0800

    angular1.x+es6开发架子

--- component/test2/index.js
+++ component/test2/index.js
@@ -1,1 +1,9 @@
 import angular from '../../js/angular';
+import controller from './tabCtrl';
+
+const ddo = {
+	restrict: 'E',
+	controller: controller
+};
+
+export default angular.module('component', []).directive('tab', () => ddo).name;
---
+++ component/test2/tabCtrl.js
@@ -0,0 +1,6 @@
+export default class tabCtrl {
+	constructor($scope) {
+		this._$scope = $scope;
+	}
+}
+tabCtrl.$inject = ['$scope'];
